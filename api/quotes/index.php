3bf9e47c99bc8886159ce46574c74ca4f47b3ded
Autumn Hubbell <autumnmhubbell@gmail.com>
2025-03-26 17:34:07 -0500

    Fixed OPTIONS header

--- api/quotes/index.php
+++ api/quotes/index.php
@@ -3,6 +3,11 @@
     header('Content-Type: application/json');
     $method = $_SERVER['REQUEST_METHOD'];
 
+    if ($method === 'OPTIONS') {
+        header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
+        header('Access-Control-Allow-Headers: Origin, Accept, Content-Type, X-Requested-With');
+        exit();
+    }
 
     if ($method === 'GET') {
         header('Access-Control-Allow-Methods: GET');
