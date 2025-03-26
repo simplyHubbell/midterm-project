297f6e8e78a0077f26d3695a11c1cd5876c68aa7
Autumn Hubbell <autumnmhubbell@gmail.com>
2025-03-26 17:36:45 -0500

    Added OPTIONS headers

--- api/authors/index.php
+++ api/authors/index.php
@@ -4,6 +4,12 @@
     $method = $_SERVER['REQUEST_METHOD'];
 
 
+    if ($method === 'OPTIONS') {
+        header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
+        header('Access-Control-Allow-Headers: Origin, Accept, Content-Type, X-Requested-With');
+        exit();
+    }
+    
     if ($method === 'GET') {
         header('Access-Control-Allow-Methods: GET');
         header('Access-Control-Allow-Headers: Origin, Accept, Content-Type, X-Requested-With');
